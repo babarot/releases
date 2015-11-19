a52340cc5990f1736201c6726556ac1aec8adde6
b4b4r07 <b4b4r07@gmail.com>
2015-11-19 11:15:24 +0900

    :tada: alpha version 1

--- release.sh
+++ release.sh
@@ -1,45 +1,42 @@
 #!/bin/bash
 
+trap 'cleanup; exit 1' ERR INT
+
+die() {
+    echo "$@" >&2
+    exit 1
+}
+
+cleanup() {
+    rm -f *.zip *.tar.gz *.tgz
+}
+
 # Logging stuff.
 e_header()   { echo -e "\n\033[1m$@\033[m"; }
-e_success()  { echo -e " \033[1;32m\U2B55\033[m  $@"; }
-e_error()    { echo -e " \033[1;31m\U274C\033[m  $@"; }
 e_arrow()    { echo -e " \033[1;34m=>\033[m  $@"; }
-
-# OS detection
-is_osx() {
-    [[ "$OSTYPE" =~ ^darwin ]] || return 1
-}
-is_ubuntu() {
-    [[ "$(cat /etc/issue 2> /dev/null)" =~ Ubuntu ]] || return 1
-}
-get_os() {
-    for os in osx ubuntu; do
-        is_$os; [[ $? == ${1:-0} ]] && echo $os
-    done
-}
+e_success()  { echo -e " [\033[1;31mx\033[m]  $@"; }
+e_error()    { echo -e " [\033[1;31m \033[m]  $@"; }
 
 # Display a fancy multi-select menu.
 # Inspired by http://serverfault.com/a/298312
 prompt_menu() {
-    local exitcode prompt nums
-    exitcode=0
-    if [[ "$exitcode" == 0 ]]; then
-        prompt="Toggle options (Separate options with spaces, ENTER when done): "
-        while clear; _prompt_menu_draws "$1" 1 && read -rp "$prompt" nums && [[ "$nums" ]]
-        do
-            _prompt_menu_adds $nums
-        done
-    fi 1>&2
+    local prompt nums
+    prompt="Toggle options (Separate options with spaces, ENTER when done): "
+    while clear; _prompt_menu_draws "$1" 1 && read -rp "$prompt" nums && [[ "$nums" ]]
+    do
+        _prompt_menu_adds $nums
+    done 1>&2
     _prompt_menu_adds
 }
 
 _prompt_menu_iter() {
     local i sel state
     local fn=$1; shift
-    for i in "${!menu_options[@]}"; do
+    for i in "${!menu_options[@]}"
+    do
         state=0
-        for sel in "${menu_selects[@]}"; do
+        for sel in "${menu_selects[@]}"
+        do
             [[ "$sel" == "${menu_options[i]}" ]] && state=1 && break
         done
         $fn $state $i "$@"
@@ -70,7 +67,8 @@ _prompt_menu_add() {
     local state i n keep match
     state=$1; shift
     i=$1; shift
-    for n in "$@"; do
+    for n in "$@"
+    do
         if [[ $n =~ ^[0-9]+$ ]] && (( n-1 == i )); then
             match=1; [[ "$state" == 0 ]] && keep=1
         fi
@@ -79,15 +77,15 @@ _prompt_menu_add() {
     _prompt_menu_result=("${_prompt_menu_result[@]}" "${menu_options[i]}")
 }
 
-init_files() {
-    local i f dirname
+releases_list() {
+    local i f q
     f=("$@")
     menu_options=(); menu_selects=()
     for i in "${!f[@]}"
     do
         menu_options[i]="$(basename "${f[i]}")"
     done
-    prompt_menu "Run the following init scripts?" $prompt_delay
+    prompt_menu "Which binaries do you download?" $prompt_delay
     for i in "${!menu_selects[@]}"
     do
         dir=$(
@@ -100,25 +98,38 @@ init_files() {
     done
 }
 
-list=($(
-curl -sSf -L https://github.com/b4b4r07/gomi/releases/latest \
-    | egrep -o '/b4b4r07/gomi/releases/download/[^"]*'
-curl -sSf -L https://github.com/peco/peco/releases/latest \
-    | egrep -o '/peco/peco/releases/download/[^"]*'
-))
-clear
-files=($(init_files "${list[@]}"))
+main() {
+    # Check L
+    [[ $L ]] || die "please specify user/repo as the L variable"
+    [[ $L =~ ^[A-Za-z0-9._-]+/[A-Za-z0-9._-]+$ ]] || die "$L: L consists of username/reponame"
 
-for f in "${files[@]}"
-do
-    wget "$f"
-    case "$f" in
-        *.zip)
-            unzip "${f##*/}"
-            ;;
-        *.tar.gz)
-            tar xvf "${f##*/}"
-            ;;
-    esac
-    rm -f *.zip *.tar.gz
-done
+    local list files
+    list=($(
+    curl -sSf -L https://github.com/$L/releases/latest \
+        | egrep -o '/'"$L"'/releases/download/[^"]*'
+    ))
+
+    if (( ${#list} < 1 )); then
+        die "$L: there are no available releases"
+    fi
+
+    clear
+    files=($(releases_list "${list[@]}"))
+
+    local f
+    for f in "${files[@]}"
+    do
+        wget "$f"
+        case "$f" in
+            *.zip)
+                unzip "${f##*/}"
+                ;;
+            *.tar.gz|*.tgz)
+                tar xvf "${f##*/}"
+                ;;
+        esac
+        cleanup
+    done
+}
+
+main
